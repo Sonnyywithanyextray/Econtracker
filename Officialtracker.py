c7b9d1f4e2a40bcd447b0e35041214981a684bc0
Etelson Alcius <69489350+Sonnyywithanyextray@users.noreply.github.com>
2022-12-23 20:09:27 -0500

    Update Officialtracker.py

--- Officialtracker.py
+++ Officialtracker.py
@@ -1,61 +1,124 @@
-import ipywidgets as widgets
+import sys
+import matplotlib
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QComboBox, QCheckBox
+from matplotlib.backends.backend_qt5agg import FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
-from IPython.display import display
 
-#  Dictionary containing GDP data for each country
+#Dictionary containing GDP data for each country
+
 gdp_data_by_country = {
-    "USA": {"nominal_gdp": 22996100, "ppp_gdp": 22996100},
-    "CHN": {"nominal_gdp": 17734063, "ppp_gdp": 27312548},
-    "JPN": {"nominal_gdp": 4937422, "ppp_gdp": 5396819},
-    "DEU": {"nominal_gdp": 4223116, "ppp_gdp": 4815479},
-    "GBR": {"nominal_gdp": 3186860, "ppp_gdp": 3344468},
-    "IND": {"nominal_gdp": 3173398, "ppp_gdp": 10218573},
-    "FRA": {"nominal_gdp": 2937473, "ppp_gdp": 3424152},
-    "ITA": {"nominal_gdp": 2099880, "ppp_gdp": 2713266},
-    "CAN": {"nominal_gdp": 1990762, "ppp_gdp": 1990762},
-    "KOR": {"nominal_gdp": 1798534, "ppp_gdp": 2427791},
-    "RUS": {"nominal_gdp": 1775800, "ppp_gdp": 4785445},
-    "BRA": {"nominal_gdp": 1608981, "ppp_gdp": 3435882},
-    "AUS": {"nominal_gdp": 1542660, "ppp_gdp": 1542660},
-    "ESP": {"nominal_gdp": 1425277, "ppp_gdp": 1425277},
-    "MEX": {"nominal_gdp": 1293038, "ppp_gdp": 2609993},
+"USA": {"nominal_gdp": 22996100, "ppp_gdp": 22996100},
+"CHN": {"nominal_gdp": 17734063, "ppp_gdp": 27312548},
+"JPN": {"nominal_gdp": 4937422, "ppp_gdp": 5396819},
+"DEU": {"nominal_gdp": 4223116, "ppp_gdp": 4815479},
+"GBR": {"nominal_gdp": 3186860, "ppp_gdp": 3344468},
+"IND": {"nominal_gdp": 3173398, "ppp_gdp": 10218573},
+"FRA": {"nominal_gdp": 2937473, "ppp_gdp": 3424152},
+"ITA": {"nominal_gdp": 2099880, "ppp_gdp": 2713266},
+"CAN": {"nominal_gdp": 1990762, "ppp_gdp": 1990762},
+"KOR": {"nominal_gdp": 1798534, "ppp_gdp": 2427791},
+"RUS": {"nominal_gdp": 1775800, "ppp_gdp": 4785445},
+"BRA": {"nominal_gdp": 1608981, "ppp_gdp": 3435882},
+"AUS": {"nominal_gdp": 1542660, "ppp_gdp": 1542660},
+"ESP": {"nominal_gdp": 1425277, "ppp_gdp": 1425277},
+"MEX": {"nominal_gdp": 1293038, "ppp_gdp": 2609993},
 }
 
-# Function to extract the GDP data for a given metric and a list of countries
+#Function to extract the GDP data for a given metric and a list of countries
+
 def extract_gdp_data(gdp_metric, countries):
- gdp_data_list = []
- for country in countries:
-    gdp_data_list.append(gdp_data_by_country[country][gdp_metric])
+    gdp_data_list = []
+    for country in countries:
+        try:
+            gdp_data_list.append(gdp_data_by_country[country][gdp_metric])
+        except KeyError:
+            gdp_data_list.append(0)
     return gdp_data_list
 
-# Create a dropdown widget for selecting the GDP metric to compare
-gdp_metric_select = widgets.Dropdown(
-  options=["nominal_gdp", "ppp_gdp"],
-  value="nominal_gdp",
-  description="GDP metric:",
-  disabled=False,
-)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-# Create checkboxes for selecting the countries to compare
-country_select = {}
-for country in gdp_data_by_country.keys():
-    country_select[country] = widgets.Checkbox(
-        value=True, description=country, disabled=False
-    )
+# Set the window title and size
+        self.setWindowTitle("Economic Visualization Tool")
+        self.resize(800, 600)
 
-# Function to update the bar chart with new data based on the selected GDP metric and countries
-def update_plot(gdp_metric, countries):
-    gdp_data = extract_gdp_data(gdp_metric, countries)
-    ax.clear()
-    ax.bar(countries, gdp_data)
+ # Create the main widget and set it as the central widget of the main window
+        self.main_widget = MainWidget(self)
+        self.setCentralWidget(self.main_widget)
 
-# Create the bar chart
-fig, ax = plt.subplots()
+    def on_close(self):
+       
+        sys.exit()
 
-# Display the widgets
-display(gdp_metric_select)
-for country in country_select.values():
-    display(country)
+class MainWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        #Create the GDP metric dropdown
+        self.gdp_metric_dropdown = QComboBox()
+        # Add items to the GDP metric dropdown
+        self.gdp_metric_dropdown.addItem("nominal_gdp")
+        self.gdp_metric_dropdown.addItem("ppp_gdp")
 
-# Connect the widgets to the update function
-widgets.interactive(update_plot, gdp_metric=gdp_metric_select, countries=country_select)
+        # Connect the GDP metric dropdown's currentIndexChanged signal to the on_gdp_metric_selected function
+        self.gdp_metric_dropdown.currentIndexChanged.connect(self.on_gdp_metric_selected)
+
+        # Create the country checkboxes
+        self.country_checkboxes = {}
+        for country in gdp_data_by_country:
+            self.country_checkboxes[country] = QCheckBox(country)
+
+        # Set up the layout
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("Select GDP metric:"))
+        layout.addWidget(self.gdp_metric_dropdown)
+        layout.addWidget(QLabel("Select countries:"))
+        for checkbox in self.country_checkboxes.values():
+            layout.addWidget(checkbox)
+        self.setLayout(layout)
+
+        # Create the Matplotlib figure and canvas
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        # Add the Matplotlib canvas to the layout
+        layout.addWidget(self.canvas)
+
+        # Create the Matplotlib toolbar
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        layout.addWidget(self.toolbar)
+
+    def on_gdp_metric_selected(self):
+        # Get the selected GDP metric
+        gdp_metric = self.gdp_metric_dropdown.currentText()
+
+        # Get the list of selected countries
+        selected_countries = []
+        for country, checkbox in self.country_checkboxes.items():
+            if checkbox.isChecked():
+                selected_countries.append(country)
+
+        # Extract the GDP data for the selected metric and countries
+        gdp_data = extract_gdp_data(gdp_metric, selected_countries)
+
+        # Clear the current figure
+        self.figure.clear()
+
+        # Create a new axis
+        ax = self.figure.add_subplot(111)
+
+        # Plot the GDP data
+        ax.bar(selected_countries, gdp_data)
+
+        # Set the y-axis label
+        ax.set_ylabel(gdp_metric)
+
+        # Redraw the canvas
+        self.canvas.draw()
+
+if __name__ == "__main__":
+    matplotlib.use("Qt5Agg")
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
