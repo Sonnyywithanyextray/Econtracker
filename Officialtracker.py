2d24a934a57559df00da2fc618b0e030f338d0df
Etelson Alcius <69489350+Sonnyywithanyextray@users.noreply.github.com>
2022-12-24 17:46:27 -0500

    Update Officialtracker.py

--- Officialtracker.py
+++ Officialtracker.py
@@ -1,6 +1,7 @@
 import sys
 import matplotlib
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QComboBox, QCheckBox
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QComboBox, QCheckBox, QGraphicsOpacityEffect
 from matplotlib.backends.backend_qt5agg import FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 
@@ -38,18 +39,47 @@ def extract_gdp_data(gdp_metric, countries):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-# Set the window title and size
+        
+        # Create welcome screen widget
+        self.welcome_widget = QWidget()
+        self.welcome_widget.setStyleSheet("background-color: black;")
+        
+        # Set the window icon
+        self.setWindowIcon(QtGui.QIcon('globalw.png'))
+        # Set the window title and size
         self.setWindowTitle("Economic Visualization Tool")
         self.resize(800, 600)
-
- # Create the main widget and set it as the central widget of the main window
+        
+        # Create the main widget and set it as the central widget of the main window
         self.main_widget = MainWidget(self)
+        
+        # Add welcome screen widget as the central widget
+        self.setCentralWidget(self.welcome_widget)
+        
+        # Create welcome label and add it to the welcome screen widget
+        self.welcome_label = QLabel("Welcome to the Economic Visualization Tool")
+        self.welcome_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.welcome_label.setStyleSheet("color: white; font-size: 24px;")
+        self.welcome_widget.layout = QVBoxLayout()
+        self.welcome_widget.layout.addWidget(self.welcome_label)
+        self.welcome_widget.setLayout(self.welcome_widget.layout)
+        
+        # Create fade animation for welcome screen
+        self.fade_animation = QtCore.QPropertyAnimation(self.welcome_widget, b"windowOpacity")
+        self.fade_animation.setDuration(3000)
+        self.fade_animation.setStartValue(1)
+        self.fade_animation.setEndValue(0)
+        self.fade_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuad)
+        
+        # Connect fade animation finished signal to show_main_widget slot
+        self.fade_animation.finished.connect(self.show_main_widget)
+        
+        # Start fade animation
+        self.fade_animation.start()
+        
+    def show_main_widget(self):
+        # Set main widget as the central widget
         self.setCentralWidget(self.main_widget)
-
-    def on_close(self):
-       
-        sys.exit()
 
 class MainWidget(QWidget):
     def __init__(self, parent):
@@ -59,66 +89,110 @@ class MainWidget(QWidget):
         # Add items to the GDP metric dropdown
         self.gdp_metric_dropdown.addItem("nominal_gdp")
         self.gdp_metric_dropdown.addItem("ppp_gdp")
-
+        
         # Connect the GDP metric dropdown's currentIndexChanged signal to the on_gdp_metric_selected function
         self.gdp_metric_dropdown.currentIndexChanged.connect(self.on_gdp_metric_selected)
-
+        
         # Create the country checkboxes
         self.country_checkboxes = {}
         for country in gdp_data_by_country:
             self.country_checkboxes[country] = QCheckBox(country)
-
-        # Set up the layout
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Select GDP metric:"))
-        layout.addWidget(self.gdp_metric_dropdown)
-        layout.addWidget(QLabel("Select countries:"))
+        
+        # Connect the country checkboxes' stateChanged signal to the on_country_selected function
         for checkbox in self.country_checkboxes.values():
-            layout.addWidget(checkbox)
-        self.setLayout(layout)
-
-        # Create the Matplotlib figure and canvas
+            checkbox.stateChanged.connect(self.on_country_selected)
+        
+        # Create the figure and canvas to display the GDP data
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-
-        # Add the Matplotlib canvas to the layout
-        layout.addWidget(self.canvas)
-
-        # Create the Matplotlib toolbar
+        
+        # Create the navigation toolbar
         self.toolbar = NavigationToolbar(self.canvas, self)
-        layout.addWidget(self.toolbar)
+        
+        # Create the layout and add the GDP metric dropdown, country checkboxes, canvas, and toolbar
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.gdp_metric_dropdown)
+        for checkbox in self.country_checkboxes.values():
+            self.layout.addWidget(checkbox)
+        self.layout.addWidget(self.canvas)
+        self.layout.addWidget(self.toolbar)
+        self.setLayout(self.layout)
+        # Create fade animation for welcome screen
+        self.fade_animation = QtCore.QPropertyAnimation(self.welcome_widget, b"windowOpacity")
+        self.fade_animation.setDuration(3000)
+        self.fade_animation.setStartValue(1)
+        self.fade_animation.setEndValue(0)
+        self.fade_animation.setEasingCurve(QtCore.QEasingCurve.InOutQuad)
+        
+        # Connect fade animation finished signal to show_main_widget slot
+        self.fade_animation.finished.connect(self.show_main_widget)
+        
+        # Start fade animation
+        self.fade_animation.start()
+        
+    def show_main_widget(self):
+        # Set main widget as the central widget
+        self.setCentralWidget(self.main_widget)
 
+class MainWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        #Create the GDP metric dropdown
+        self.gdp_metric_dropdown = QComboBox()
+        # Add items to the GDP metric dropdown
+        self.gdp_metric_dropdown.addItem("nominal_gdp")
+        self.gdp_metric_dropdown.addItem("ppp_gdp")
+        
+        # Connect the GDP metric dropdown's currentIndexChanged signal to the on_gdp_metric_selected function
+        self.gdp_metric_dropdown.currentIndexChanged.connect(self.on_gdp_metric_selected)
+        
+        # Create the country checkboxes
+        self.country_checkboxes = {}
+        for country in gdp_data_by_country:
+            self.country_checkboxes[country] = QCheckBox(country)
+        
+        # Connect the country checkboxes' stateChanged signal to the on_country_selected function
+        for checkbox in self.country_checkboxes.values():
+            checkbox.stateChanged.connect(self.on_country_selected)
+        
+        # Create the figure and canvas to display the GDP data
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        
+        # Create the navigation toolbar
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        
+        # Create the layout and add the GDP metric dropdown, country checkboxes, canvas, and toolbar
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.gdp_metric_dropdown)
+        for checkbox in self.country_checkboxes.values():
+            self.layout.addWidget(checkbox)
+        self.layout.addWidget(self.canvas)
+        self.layout.addWidget(self.toolbar)
+        self.setLayout(self.layout)
+        # Initialize the plot with nominal GDP data for all countries
+        self.on_gdp_metric_selected()
+        
     def on_gdp_metric_selected(self):
-        # Get the selected GDP metric
+        # Extract the GDP data for the selected metric and all selected countries
         gdp_metric = self.gdp_metric_dropdown.currentText()
-
-        # Get the list of selected countries
-        selected_countries = []
-        for country, checkbox in self.country_checkboxes.items():
-            if checkbox.isChecked():
-                selected_countries.append(country)
-
-        # Extract the GDP data for the selected metric and countries
+        selected_countries = [country for country, checkbox in self.country_checkboxes.items() if checkbox.isChecked()]
         gdp_data = extract_gdp_data(gdp_metric, selected_countries)
-
-        # Clear the current figure
+        
+        # Clear the figure and plot the GDP data
         self.figure.clear()
-
-        # Create a new axis
         ax = self.figure.add_subplot(111)
-
-        # Plot the GDP data
         ax.bar(selected_countries, gdp_data)
-
-        # Set the y-axis label
         ax.set_ylabel(gdp_metric)
-
-        # Redraw the canvas
         self.canvas.draw()
+        
+    def on_country_selected(self):
+        # Update the plot when a country checkbox is selected
+        self.on_gdp_metric_selected()
 
-if __name__ == "__main__":
-    matplotlib.use("Qt5Agg")
+# Run the application
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
